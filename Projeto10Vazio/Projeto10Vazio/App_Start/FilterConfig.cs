0bb5c63fef265280abebc281397af1c52008feb1
3Amodular <3mod@bglab.local>
2023-08-21 14:29:09 -0300

    Commit 1.0 com a estrutura base do ASP.NET

---
+++ Projeto10Vazio/Projeto10Vazio/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto10Vazio
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Projeto10Vazio/Projeto10Vazio/Dados/conexao.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Projeto10Vazio.Dados
+{
+    public class conexao
+    {
+
+
+
+
+    }
+}
