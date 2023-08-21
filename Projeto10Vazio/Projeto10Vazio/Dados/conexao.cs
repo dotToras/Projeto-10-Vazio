ec65dc6ea82bdc6a71e26ae2d969082ec248fbe8
3Amodular <3mod@bglab.local>
2023-08-21 14:41:10 -0300

    Versão 1.1 do ASP.NET, com a conexão e a classe já criadas

--- Projeto10Vazio/Projeto10Vazio/Dados/conexao.cs
+++ Projeto10Vazio/Projeto10Vazio/Dados/conexao.cs
@@ -12,6 +12,43 @@ namespace Projeto10Vazio.Dados
 
 
 
+        MySqlConnection cn = new MySqlConnection("Server=localhost;DataBase=BD; User=root; pwd=12345678");
+        public static string msg;
+
+
+
+        public MySqlConnection MyConnectBd() /*Método para Conectar*/
+        {
+
+            try
+            {
+                cn.Open();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+
+            }
+            return cn;
+        }
+
+        public MySqlConnection MyDesConnectBd() /*Método para desconectar*/
+        {
+
+            try
+            {
+                cn.Close();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao se desconectar" + erro.Message;
+
+            }
+            return cn;
+        }
+
+
+
 
     }
 }
--- Projeto10Vazio/Projeto10Vazio/Models/modelLogin.cs
+++ Projeto10Vazio/Projeto10Vazio/Models/modelLogin.cs
@@ -8,6 +8,11 @@ namespace Projeto10Vazio.Models
     public class modelLogin
     {
 
-       
+        public int Cod { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
     }
 }
